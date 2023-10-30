2ff7bbfb44e01df90bdf89b505be34778b259686
AlimChowdhury <md.alim.chowdhury8@gmail.com>
2023-10-30 11:28:10 +0600

    add utils

---
+++ src/main/java/com/example/travelblog/utils/BlogItemTemplate.java
@@ -0,0 +1,31 @@
+package com.example.travelblog.utils;
+
+public class BlogItemTemplate {
+    private String id;
+    private String userName;
+
+    public BlogItemTemplate(String id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public BlogItemTemplate() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
+
